855e044bdb8911cc094f68faf27cdcf8ea4ecf02
Jeff Smith <toxicglados@gmail.com>
2022-04-18 22:53:10 -0500

    Auto create claimer screep when claim flag is present

--- spawn.js
+++ spawn.js
@@ -1,3 +1,10 @@
+function isClaimer(creep, _index, _collection) {
+    if (!creep.memory.task) {
+        return false
+    }
+    return creep.memory.task.action == "claim"
+}
+
 function spawn() {
     var main_spawner = Game.spawns["Spawn0"];
     var main_room = main_spawner.room;
@@ -7,11 +14,21 @@ function spawn() {
     else if(Object.keys(Game.creeps).length < 10){
         var body = [];
         var energyCapacity = main_room.energyCapacityAvailable
-        for(var i = 0; i < Math.floor(energyCapacity / 200); i++){ 
-            body.push(MOVE);
-            body.push(CARRY);
-            body.push(WORK);
+
+        if (Game.flags["claim"] != null && energyCapacity > BODYPART_COST["claim"] + BODYPART_COST["move"]) {
+            if (!_.some(Game.creeps, isClaimer)) {
+                body.push(CLAIM);
+                body.push(MOVE);
+            }
         }
+        else {
+            for(var i = 0; i < Math.floor(energyCapacity / 200); i++){
+                body.push(MOVE);
+                body.push(CARRY);
+                body.push(WORK);
+            }
+        }
+
         var err = main_spawner.spawnCreep(body, "C" + parseInt(Math.floor(Math.random() * 10000)));
     }
 }
