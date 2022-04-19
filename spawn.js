85b3623c2e760e34c4f1b42bb72a6b8f32f0a763
Jeff Smith <toxicglados@gmail.com>
2022-04-19 14:37:36 -0500

    Spawn from all spawners, and also heal nearby creeps when possible

--- spawn.js
+++ spawn.js
@@ -6,30 +6,39 @@ function isClaimer(creep, _index, _collection) {
 }
 
 function spawn() {
-    var main_spawner = Game.spawns["Spawn0"];
-    var main_room = main_spawner.room;
-    if(Object.keys(Game.creeps).length < 3){
-        var err = main_spawner.spawnCreep([MOVE, WORK, CARRY], "C" + parseInt(Math.floor(Math.random() * 10000)));
-    }
-    else if(Object.keys(Game.creeps).length < 13){
-        var body = [];
-        var energyCapacity = main_room.energyCapacityAvailable
+    for (var spawner_name in Game.spawns) {
+        var spawner = Game.spawns[spawner_name];
+        var room = spawner.room;
 
-        if (Game.flags["claim"] != null && energyCapacity > BODYPART_COST["claim"] + BODYPART_COST["move"]) {
-            if (!_.some(Game.creeps, isClaimer)) {
-                body.push(CLAIM);
-                body.push(MOVE);
-            }
-        }
-        else {
-            for(var i = 0; i < Math.floor(energyCapacity / 200); i++){
-                body.push(MOVE);
-                body.push(CARRY);
-                body.push(WORK);
-            }
+        var adjacent_creeps = spawner.pos.findInRange(FIND_MY_CREEPS, 1);
+        if (adjacent_creeps.length > 0) {
+            var creep = adjacent_creeps[0];
+            var err = spawner.renewCreep(creep);
         }
 
-        var err = main_spawner.spawnCreep(body, "C" + parseInt(Math.floor(Math.random() * 10000)));
+        if(Object.keys(Game.creeps).length < 3){
+            var err = spawner.spawnCreep([MOVE, WORK, CARRY], "C" + parseInt(Math.floor(Math.random() * 10000)));
+        }
+        else if(Object.keys(Game.creeps).length < 13){
+            var body = [];
+            var energyCapacity = room.energyCapacityAvailable
+
+            if (Game.flags["claim"] != null && energyCapacity > BODYPART_COST["claim"] + BODYPART_COST["move"]) {
+                if (!_.some(Game.creeps, isClaimer)) {
+                    body.push(CLAIM);
+                    body.push(MOVE);
+                }
+            }
+            else {
+                for(var i = 0; i < Math.floor(energyCapacity / 200); i++){
+                    body.push(MOVE);
+                    body.push(CARRY);
+                    body.push(WORK);
+                }
+            }
+
+            var err = spawner.spawnCreep(body, "C" + parseInt(Math.floor(Math.random() * 10000)));
+        }
     }
 }
 
