5d1b6b657b6fc2f8cbfabe9b87e46484923d2f9d
Jeff Smith <toxicglados@gmail.com>
2022-04-24 23:13:37 -0500

    Spawn best defender we can when an enemy is here

--- spawn.js
+++ spawn.js
@@ -9,17 +9,34 @@ function spawn() {
     for (var spawner_name in Game.spawns) {
         var spawner = Game.spawns[spawner_name];
         var room = spawner.room;
+        var energyCapacity = room.energyCapacityAvailable
 
         var enemies = room.find(FIND_HOSTILE_CREEPS);
+        // Spawn the biggest guy we can
         if(enemies.length > 0){
-            var err = spawner.spawnCreep([MOVE, MOVE, ATTACK, ATTACK], "C" + parseInt(Math.floor(Math.random() * 10000)));
+            var bodypart_cost_sum = BODYPART_COST[TOUGH] + BODYPART_COST[MOVE] + BODYPART_COST[ATTACK];
+            var energy_available = room.energyAvailable;
+            var body = [];
+
+            if (energy_available >= bodypart_cost_sum) {
+                var num_each_bodyparts = Math.floor(energy_available / bodypart_cost_sum);
+                for (var i = 0; i < num_each_bodyparts; i++) {
+                    body.push(TOUGH);
+                }
+                for (var i = 0; i < num_each_bodyparts; i++) {
+                    body.push(ATTACK);
+                }
+                for (var i = 0; i < num_each_bodyparts; i++) {
+                    body.push(MOVE);
+                }
+                var err = spawner.spawnCreep(body, "C" + parseInt(Math.floor(Math.random() * 10000)));
+            }
         }
         else if(Object.keys(Game.creeps).length < 3){
             var err = spawner.spawnCreep([MOVE, WORK, CARRY], "C" + parseInt(Math.floor(Math.random() * 10000)));
         }
         else if(Object.keys(Game.creeps).length < 18){
             var body = [];
-            var energyCapacity = room.energyCapacityAvailable
 
             if (Game.flags["claim"] != null && energyCapacity > BODYPART_COST["claim"] + BODYPART_COST["move"]) {
                 if (!_.some(Game.creeps, isClaimer)) {
